7257fdb23ce4831e53d2d14027ae4e385fc2af5c
venik <venik@venik.org>
2014-03-03 23:03:04 +0100

    camera-shot check light (wemo switch)

--- chilli/camera-shot.sh
+++ chilli/camera-shot.sh
@@ -7,8 +7,18 @@ script_start $*
 
 FILE=$(date +%Y%m%d%H%M).jpg
 
+./wemo.sh status
+RETVAL=$?
+if [ "$RETVAL" -ne 1 ]; then
+	debug "Light is off, no camera shots..."
+	script_end
+	exit -1
+fi
+
+debug "Taking camera shot: $FILE"
 wget $CAMERA_SHOT_URL -O - -a ${LOGDIR}/camera-shot.log | convert - -transpose -transpose -rotate 180 ${SHOTSDIR}/${FILE}
 
+debug "Copying file to remote location"
 cp ${SHOTSDIR}/${FILE} /mnt/home/Dropbox/Photos/Chilli
 
 script_end
