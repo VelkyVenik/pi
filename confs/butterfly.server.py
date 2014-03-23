739de0dc344c8fe7407b5b8bec3d659ce0605e1c
venik <venik@venik.org>
2014-03-23 10:56:45 +0100

    config update

---
+++ confs/butterfly.server.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'butterfly==1.2.4','butterfly.server.py'
+__requires__ = 'butterfly==1.2.4'
+import pkg_resources
+pkg_resources.run_script('butterfly==1.2.4', 'butterfly.server.py')
